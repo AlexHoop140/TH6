8d5bfd2fed83164fbdd020950bfbadafbbc7486b
AlexHoop140 <alexhoop.pro@gmail.com>
2021-11-07 02:50:51 +0700

    finally found bugs in isMember() and init_table()

--- hashtable.cpp
+++ hashtable.cpp
@@ -10,13 +10,50 @@ struct Node{
     Node* pNext;
 };
 
-typedef Node* position;
-typedef position hashTable[hashGroup];
+struct list{
+    Node* pHead;
+    Node* pTail;
+};
+
+// typedef list position;
+// typedef position hashTable[hashGroup];
+
+typedef list *hashTable;
+
+void create_List(list &linkedlist){
+    linkedlist.pHead = new Node();
+    linkedlist.pHead->pNext = linkedlist.pTail = NULL;
+}
+
+void addHead(list &l, Node *new_ele){
+    if(l.pHead->pNext == NULL){
+        l.pHead->pNext = l.pTail = new_ele;
+    }
+    else{
+        new_ele->pNext = l.pHead->pNext;
+        l.pHead->pNext = new_ele;
+    }
+}
+
+void addLast(list &l, Node *new_ele){
+    if(l.pTail == NULL){
+        l.pHead->pNext = new_ele;
+        l.pTail = new_ele;
+    }
+    else{
+        l.pTail->pNext = new_ele;
+        new_ele->pNext = NULL;
+        l.pTail = new_ele;
+    }
+}
 
 void init_Table(hashTable &table){
+    table = new list[hashGroup];
     for(int i = 0; i < hashGroup; i++){
-        table[i] = new Node();
+        create_List(table[i]);
     }
+
+    
 }
 
 int hashFunction(dataType x){
@@ -24,11 +61,12 @@ int hashFunction(dataType x){
 }
 
 int isMember(dataType x, hashTable &table){
-    position p = table[hashFunction(x)];
+    Node *p = table[hashFunction(x)].pHead;
 
-    while(p != NULL){
-        if(p->data == x)
+    while(p->pNext != NULL){
+        if(p->pNext->data == x)
             return 1;
+        p = p->pNext;
     }
     return 0;
 }
@@ -43,8 +81,8 @@ Node* getNode(dataType x){
 }
 
 void insertItem(dataType x, hashTable &table){
-    int bucket;
-    position p;
+    // int bucket;
+    // Node *p;
 
     // if(!isMember(x, table)){
     //     bucket = hashFunction(x);
@@ -54,40 +92,53 @@ void insertItem(dataType x, hashTable &table){
     //     table[bucket]->pNext = p;
     //     p->pNext = NULL;
     // }
+    int bucket = hashFunction(x);
+    if(isMember(x, table) == false){
+        // cout << "ADD: " << x << endl;
+        addLast(table[bucket], getNode(x));
+    }
+}
+
+void deleteList(Node *position, list &l){
+	Node *temp;
+	if(position->pNext != NULL){
+		temp = position->pNext;
+		position->pNext = temp->pNext;
+		delete temp;
+	}
 }
 
 void removeItem(dataType x, hashTable &table){
     int bucket;
-    position p, temp;
+    Node *p, *temp;
     bucket = hashFunction(x);
-    if(table[bucket] != NULL){
-        if(table[bucket]->data == x){
-            temp = table[bucket];
-            table[bucket] = temp->pNext;
-            delete temp;
+    if(table[bucket].pHead->pNext != NULL){
+        if(table[bucket].pHead->pNext->data == x){
+            deleteList(table[bucket].pHead->pNext, table[bucket]);
             cout << "Removed!" << endl;
         } else{
-            p = table[bucket];
-            while(p->pNext->data != x && p->pNext != NULL){
-                p = p->pNext;
-            }
-            if(p->pNext->data == x){
-                temp = p->pNext;
-                p->pNext = temp->pNext;
-                delete temp;
-                cout << "Removed!" << endl;
-            } else {
-                if(p->pNext == NULL)
-                    cout << "Not found" << endl;
-            }
-        }
+            Node *p = table[bucket].pHead;
+		    while(p->pNext != NULL){
+			    if(p->pNext->data == x){
+				    deleteList(p, table[bucket]);
+				    return;
+			    }
+			    p = p->pNext;
+		    }
+
+	        if(p == NULL)
+	            cout << endl << "[ERROR] CANNOT FIND ELEMENT: " << x << endl;
+	    }
+    }else{
+        cout << "[ERROR] Empty bucket!" << endl;
     }
 }
+
 
 void traverse(hashTable table){
     for(int i = 0; i < hashGroup; i++){
         cout << "Bucket " << i << ": ";
-        position p = table[i];
+        Node *p = table[i].pHead->pNext;
         while(p != NULL){
             cout << p->data << " --> ";
             p = p->pNext;
@@ -103,13 +154,18 @@ int main(){
     init_Table(HT);
 
     insertItem(1, HT);
-    insertItem(8, HT);
     insertItem(27, HT);
-    // insertItem(64, HT);
-    // insertItem(125, HT);
-    // insertItem(216, HT);
-    // insertItem(343, HT);
+    insertItem(64, HT);
+    insertItem(125, HT);
+    insertItem(216, HT);
+    insertItem(343, HT);
+    insertItem(8, HT);
 
+    traverse(HT);
+
+    removeItem(216, HT);
+
+    cout << "After remove: " << endl;
     traverse(HT);
 }
 
